7cae5a8b723e39dfbf66b90dabd44f07c9d5d783
harjeet <harjeet.kalsi@solirius.com>
2023-10-31 19:33:56 +0000

    added a function in javascript

--- session-3/index.js
+++ session-3/index.js
@@ -1,14 +1,29 @@
-let a = 5; 
-const b = 10; 
+// let a = 5; 
+// const b = 10; 
 
-let c = a + b
+// let c = a + b
 
-console.log(c)
+// console.log(c)
 
-a = 20
+// a = 20
 
-console.log(c) 
+// console.log(c) 
 
-c = a + b
+// c = a + b
 
-console.log(c)
+// console.log(c)
+
+
+function sayHey () {
+    console.log("hey!")
+}
+
+
+function conversation (){
+    sayHey()
+    console.log("How are you?")
+    console.log("Goodbye!")
+}
+
+
+conversation()
