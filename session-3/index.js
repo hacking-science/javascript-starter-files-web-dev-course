a92fec11ba3b4effcec6e743362f56838f92f239
harjeet <harjeet.kalsi@solirius.com>
2023-10-31 20:01:06 +0000

    practiced writing functions

--- session-3/index.js
+++ session-3/index.js
@@ -14,16 +14,27 @@
 // console.log(c)
 
 
-function sayHey () {
-    console.log("hey!")
+function sayHey (name) {
+    console.log("hey " + name + "!")
 }
 
-
-function conversation (){
-    sayHey()
-    console.log("How are you?")
-    console.log("Goodbye!")
+function conversation(name, topic){
+    sayHey(name)
+    console.log("Do you like " + topic + "?")
 }
 
+function multiply_by_5(x){
+    return x * 5
+}
 
-conversation()
+let y = multiply_by_5(8)
+
+
+function futureAge (name, current_age){
+    age_in_5_years = current_age + 5
+    return "Hi " + name + "! You will be " + age_in_5_years + " in 5 years time"
+}
+
+console.log(futureAge("Harjeet", 27))
+
+console.log(futureAge("Mary", 17))
