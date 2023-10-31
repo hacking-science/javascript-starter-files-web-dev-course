27df3e153498dfcd3ec3eea0a303ac5b75b2fc2c
harjeet <harjeet.kalsi@solirius.com>
2023-10-31 19:12:57 +0000

    Completed task 1

--- session-3/index.js
+++ session-3/index.js
@@ -1,2 +1,14 @@
-// This is a comment in JavaScript
-// You can see where this file is connected to your HTML in the index.html on line 14
+let a = 5; 
+const b = 10; 
+
+let c = a + b
+
+console.log(c)
+
+a = 20
+
+console.log(c) 
+
+c = a + b
+
+console.log(c)
